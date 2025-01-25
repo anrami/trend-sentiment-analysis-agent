4e74a7bc68d47d80a3b3624e2c3a4f1141a8768b
Henry Addo <henry@addhen.org>
2025-01-25 13:20:05 -0800

    Add needed packages to run agent.py

--- backend/trendsenti/app/src/agent.py
+++ backend/trendsenti/app/src/agent.py
@@ -6,7 +6,6 @@ from langgraph.prebuilt import create_react_agent
 from dotenv import load_dotenv
 import os
 
-load .env
 load_dotenv()
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 TAVILY_API_KEY = os.environ["TAVILY_API_KEY"]
@@ -18,14 +17,12 @@ search = TavilySearchResults(max_results=2)
 tools = [search]
 agent_executor = create_react_agent(model, tools, checkpointer=memory)
 
-#define keyword/news sources
-sentimentKeyword = "AI Agents"
-newsSources = ['Reddit']
-formattedNewsSources = newsSources.
-#Use the agent
-config = {"configurable": {"thread_id": "abc123"}}
-for chunk in agent_executor.stream(
-    {"messages": [HumanMessage(content=f"You are a guru on sentimate analysis, can you look on these news sources: {newsSources} and report to me what the overall sentiment based on the last ten posts for {sentimentKeyword}, is it positive or negative and to what percentage?")]}, config
-):
-    print(chunk)
-    print("----")
+def get_sentiment(keywords, sources):
+    # Use the agent
+    config = {"configurable": {"thread_id": "abc123"}}
+    for chunk in agent_executor.stream(
+        {"messages": [HumanMessage(content=f"You are a guru on sentimate analysis, can you look on these news sources: {sources} and report to me what the overall sentiment based on the last ten posts for {keywords}, is it positive or negative and to what percentage?")]}, config
+    ):
+        print(chunk)
+        print("----")
+    return chunk
